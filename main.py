d7c7323dc38c507e17c5a821445af3d5d997d9b7
Sardor <139547140+Xakimov-Sardor@users.noreply.github.com>
2023-10-25 08:01:28 +0000

    366- 61- comlete

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ class Problem:
     def _0564(n: str) -> int:
 
         return
+
+    def _0061(n: int) -> int:
+        return n * ((n *2) - 1)
+
+    def _0366(n: int) -> int: # complete
+        if n < 3:
+            return 0
+        return (n - 2) * 180
 class main:
     def run(self):
 
-        response = Problem._0062(0)
+        response = Problem._0366(1)
         print(response)
 
 if __name__ == '__main__':
